afd394d645ffa7b0830441c5e1c0e15bc2f3b61a
JASON <jason.rbm@gmail.com>
2020-03-14 16:46:57 -0600

    First commit

---
+++ Services/IShoppingListService.cs
@@ -0,0 +1,13 @@
+﻿using LambdaShoppingList.Models;
+using System.Collections.Generic;
+
+namespace LambdaShoppingList.Services
+{
+    public interface IShoppingListService
+    {
+
+        Dictionary<string, int> GetItemFromShoppingList();
+        void AddItemsToShoppingList(ShoppingListModel shoppingList);
+        void RemoveItem(string name);
+    }
+}
---
+++ Services/ShoppingListService.cs
@@ -0,0 +1,28 @@
+﻿using LambdaShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LambdaShoppingList.Services
+{
+    public class ShoppingListService : IShoppingListService
+    {
+        public Dictionary<string, int> _shoppingListStorage = new Dictionary<string, int>();
+
+        public void AddItemsToShoppingList(ShoppingListModel shoppingList)
+        {
+            _shoppingListStorage.Add(shoppingList.Name,shoppingList.Quantity);
+        }
+
+        public Dictionary<string, int> GetItemFromShoppingList() 
+        {
+            return _shoppingListStorage;
+        }
+
+        public void RemoveItem(string name)
+        {
+            _shoppingListStorage.Remove(name);
+        }
+    }
+}
